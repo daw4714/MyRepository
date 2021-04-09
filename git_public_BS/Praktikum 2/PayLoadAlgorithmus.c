3ab07da8356b550ac13655ed904d9292355502ec
Some Student. Put your name here <your.email@your.provider>
2021-04-09 15:48:55 +0200

    Laufzeitbestimmung PayLoad Algorithmus1

--- git_public_BS/Praktikum 2/PayLoadAlgorithmus.c
+++ git_public_BS/Praktikum 2/PayLoadAlgorithmus.c
@@ -2,10 +2,10 @@
 #include <pthread.h>
  
 
-int primzahl(int  zahl){
+int primzahl1(int  zahl){
 
   for( size_t i = 2; i < zahl; i++){
-    if(x %  i = 0){
+    if(zahl % i == 0){
       return 0;
     }
    }
@@ -15,7 +15,7 @@ int primzahl(int  zahl){
 
 void * thread_fkt(void * args){
   int* primzahl = (int*) args;
-  int i =  primzahl(*primzahl);
+  int i =  primzahl1(*primzahl);
   return &i;
 }
 
@@ -25,14 +25,14 @@ int main(){
 
   for(size_t t = 0; t < istPrimzahl/2; t++){
       
-      pthread_create(&threadA, NULL, &example_fkt, &t );  
+      pthread_create(&threadA, NULL, &thread_fkt, &t );  
       pthread_join(&threadA, NULL);
   }
 
   for(size_t t = istPrimzahl/2; t <= istPrimzahl; t++){
     
-      pthread_create(&threadB, NULL, &example_fkt, &t );
-      pthread_join(&treadB, NULL);
+      pthread_create(&threadB, NULL, &thread_fkt, &t );
+      pthread_join(&threadB, NULL);
         
   }
       return 0;
