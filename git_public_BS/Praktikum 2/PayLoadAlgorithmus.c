6bfbcd616994a173afa293b722171d2dbe28e8df
Some Student. Put your name here <your.email@your.provider>
2021-04-16 15:23:19 +0200

    besprochener PayLoadAlgorithmus

--- git_public_BS/Praktikum 2/PayLoadAlgorithmus.c
+++ git_public_BS/Praktikum 2/PayLoadAlgorithmus.c
@@ -1,56 +1,75 @@
 #include <stdio.h>
+#include<stdint.h>
+#include<time.h>
+#include<stdbool.h>
 #include <pthread.h>
+#define MAX_TO_TEST (10)
 
-struct timespec {
-  time_t tv_sec;  //Sekunden
-  long   tv_nsec; //Nanosekunden
+struct parameter{
+  uint64_t offset;
+  uint64_t step;
+  uint64_t group;
 };
 
-int primzahl1(int  zahl){
-
+bool  primzahl1(int  zahl){
   for( size_t i = 2; i < zahl; i++){
     if(zahl % i == 0){
-      return 0;
+      return false;
     }
    }
-  return 1;
-  
+  return true;
  }
 
-void * thread_fkt(void * args){
-  int* primzahl = (int*) args;
-  int i =  primzahl1(*primzahl);
-  return &i;
-}
+void *calc_prime(void *args){
+ struct parameter *param = (struct parameter *) args;
 
-int main(){
-  int istPrimzahl = 100;
-  int anzahlThreads = 2;
-  pthread threadA, threadB, threadC;  
-  int clock_gettime(clockid_t clk_id, struct timespec *tp2);
-  for(size_t t = 0; t < istPrimzahl/2; t++){
-      
-      pthread_create(&threadA, NULL, &thread_fkt, &t );  
-      pthread_join(&threadA, NULL);
-  }
+  for(size_t i = 2 + param->offset; i < MAX_TO_TEST; i+= param->step){
 
-  for(size_t t = istPrimzahl/2; t <= istPrimzahl; t++){
-    
-      pthread_create(&threadB, NULL, &thread_fkt, &t );
-      pthread_join(&threadB, NULL);
-        
-  }
-  int clock_gettime(clockid_t clk_id, struct timespec *tp3);
-  for(size_t t = 0; t < istPrimzahl/2; t++){
+    for(size_t j = 0; j < param->group; ++j){
+      primzahl1(i+j);
+    }
 
-      pthread_create(&threadA, NULL, &thread_fkt, &t );
-      pthread_join(&threadA, NULL);
   }
-  int clock_gettime(clockid_t clk_id, struct timespec *tp4);
-  int speedup =(tp4->tv_sec - tp3->tv_sec)/ (tp2->tv_sec - tp1->tv_sec) +(tp4->tv_nsec - tp3->tv_nsec)/ (tp2->tv_nsec - tp1->tv_nsec);
-  printf("%d\n", speedup);
-    return 0;
+  return NULL;
 
 }
 
 
+
+
+#define NUM_THREAD (4)
+int main(void * args){
+
+uint64_t counter;
+struct parameter param[NUM_THREAD]; //Array an Structuren
+pthread_t t[NUM_THREAD]; //Array an Threads
+struct timespec start, stop, delta;
+ timespec_get(&start, TIME_UTC);
+ double d_delta;
+for( counter = 0; counter < NUM_THREAD; counter ++){
+
+  param[counter].group = 2;
+  param[counter].offset = counter * param[counter].group;
+  param[counter].step = 4 * param[counter].group;
+  pthread_create(&t[counter], NULL, &calc_prime, &param[counter]);
+
+}
+
+for( counter = 0; counter < NUM_THREAD; counter ++){
+
+  pthread_join(&t[counter], NULL);
+
+}
+
+ timespec_get(&stop, TIME_UTC);
+  delta.tv_sec = stop.tv_sec - start.tv_sec;
+ delta.tv_nsec = stop.tv_nsec - start.tv_nsec;
+ if(start.tv_nsec > stop.tv_nsec){
+   delta.tv_sec =-1;
+   delta.tv_nsec += 1000000000;
+ }
+  d_delta = (double) delta.tv_sec + (double) delta.tv_nsec/ 1000000000.0;
+  printf("%f\n", d_delta);
+return NULL;
+
+}
